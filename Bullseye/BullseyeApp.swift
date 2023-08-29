f7a12bae8175dae51a59460eabbc7ab7d96c49dc
John Phung <jpappdev021420@gmail.com>
2023-08-28 22:07:27 -0700

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Terra Riser on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
