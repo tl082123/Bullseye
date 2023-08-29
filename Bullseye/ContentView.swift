06bfe31cb2b24b406271455df58f193a18c55f15
John Phung <jpappdev021420@gmail.com>
2023-08-29 07:59:49 -0700

    Completed Chapter 5 - SwiftUI Views

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,19 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+      Text("89")
+      HStack {
+        Text("1")
+        Slider(value: .constant(50), in: 1.0...100.0)
+        Text("100")
+      }
+      Button("Hit me") {
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
