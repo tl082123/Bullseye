990d76f499cb8512e5722ae5f0542700c0f05a00
John Phung <jpappdev021420@gmail.com>
2023-09-04 14:30:44 -0700

    Completed Chapter 2 - More View Modifiers on Your First iOS & SwiftUI App: Polishing the App

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -14,12 +14,13 @@ struct ContentView: View {
   
   var body: some View {
     VStack {
-      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+      Text("🎯🎯🎯\nPut the Bullseye as close as you can to".uppercased())
         .bold()
         .multilineTextAlignment(.center)
         .lineSpacing(4.0)
         .font(.footnote)
         .kerning(2.0)
+        .padding(.horizontal, 30)
       Text(String(game.target))
         .kerning(-1.0)
         .font(.largeTitle)
@@ -31,9 +32,18 @@ struct ContentView: View {
         Text("100")
           .bold()
       }
-      Button("Hit me") {
+      .padding()
+      Button("Hit me".uppercased()) {
         alertIsVisible = true
       }
+      .padding(20.0)
+      .background(
+        Color.blue
+      )
+      .foregroundColor(.white)
+      .cornerRadius(21.0)
+      .bold()
+      .font(.title3)
       .alert(
         "Hello there!",
         isPresented: $alertIsVisible,
