a73d79d8527e0c22a2aa526c9cd2e423267e6c82
tl082123 <142849198+tl082123@users.noreply.github.com>
2023-09-05 06:02:31 -0700

    Completed Chapter 10 - Challenge: Fill & Stroke Shapes

--- Bullseye/Views/RoundedViews.swift
+++ Bullseye/Views/RoundedViews.swift
@@ -14,6 +14,10 @@ struct RoundedImageViewStroked: View {
       .font(.title)
       .foregroundColor(Color("TextColor"))
       .frame(width: 56, height: 56)
+      .overlay(
+        Circle()
+          .strokeBorder(Color("ButtonStrokeColor"), lineWidth: 2)
+      )
   }
 }
 
@@ -22,8 +26,12 @@ struct RoundedImageViewFilled: View {
   var body: some View {
     Image(systemName: systemName)
       .font(.title)
-      .foregroundColor(Color("TextColor"))
+      .foregroundColor(Color("ButtonFilledTextColor"))
       .frame(width: 56, height: 56)
+      .background(
+        Circle()
+          .fill(Color("ButtonFilledBackgroundColor"))
+      )
   }
 }
 
