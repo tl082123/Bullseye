b6b9289874a297ab730cf36bc1a1092ecddd0a1a
tl082123 <142849198+tl082123@users.noreply.github.com>
2023-09-04 18:31:02 -0700

    Completed Chapter 6 - Extract Views

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -17,19 +17,7 @@ struct ContentView: View {
       Color("BackgroundColor")
         .ignoresSafeArea()
       VStack {
-        Text("🎯🎯🎯\nPut the Bullseye as close as you can to".uppercased())
-          .bold()
-          .multilineTextAlignment(.center)
-          .lineSpacing(4.0)
-          .font(.footnote)
-          .kerning(2.0)
-          .padding(.horizontal, 30)
-          .foregroundColor(Color("TextColor"))
-        Text(String(game.target))
-          .kerning(-1.0)
-          .font(.largeTitle)
-          .fontWeight(.black)
-          .foregroundColor(Color("TextColor"))
+       InstructionsView(game: $game)
         HStack {
           Text("1")
             .bold()
@@ -73,6 +61,18 @@ struct ContentView: View {
           }
         )
       }
+    }
+  }
+}
+
+struct InstructionsView: View {
+  @Binding var game: Game
+  
+  var body: some View {
+    VStack {
+      InstructionText(text: "🎯🎯🎯\nPut the Bullseye as close as you can to")
+        .padding(.horizontal, 30)
+      BigNumberText(text: String(game.target))
     }
   }
 }
---
+++ Bullseye/Views/TextViews.swift
@@ -0,0 +1,43 @@
+//
+//  TextViews.swift
+//  Bullseye
+//
+//  Created by Terra Riser on 9/4/23.
+//
+
+import SwiftUI
+
+struct InstructionText: View {
+  var text: String
+  
+  var body: some View {
+    Text(text.uppercased())
+      .bold()
+      .multilineTextAlignment(.center)
+      .lineSpacing(4.0)
+      .font(.footnote)
+      .kerning(2.0)
+      .foregroundColor(Color("TextColor"))
+  }
+}
+
+struct BigNumberText: View {
+  var text: String
+  
+  var body: some View {
+    Text(text)
+      .kerning(-1.0)
+      .font(.largeTitle)
+      .fontWeight(.black)
+      .foregroundColor(Color("TextColor"))
+  }
+}
+
+struct TextViews_Previews: PreviewProvider {
+  static var previews: some View {
+    VStack {
+      InstructionText(text: "Instructions")
+      BigNumberText(text: "999")
+    }
+  }
+}
