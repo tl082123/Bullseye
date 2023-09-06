2667cacbc03ad108ebfc3f675fd35505e0653f39
tl082123 <142849198+tl082123@users.noreply.github.com>
2023-09-05 22:08:08 -0700

    Completed Chapter 33 - Challenge: Create a Leaderboard View

--- Bullseye/Views/RoundedViews.swift
+++ Bullseye/Views/RoundedViews.swift
@@ -53,12 +53,31 @@ struct RoundedRectTextView: View {
   }
 }
 
+struct RoundedTextView: View {
+  var text: String
+  
+  var body: some View {
+    Text(text)
+      .bold()
+      .font(.title3)
+      .foregroundColor(Color("TextColor"))
+      .frame(width: Constants.General.roundedViewLength, height: Constants.General.roundedViewLength)
+      .overlay(
+        Circle()
+          .strokeBorder(Color("LeaderboardRowColor"), lineWidth: Constants.General.strokeWidth)
+      )
+  }
+}
+
+
+
 struct PreviewView: View {
   var body: some View {
     VStack(spacing: 10) {
       RoundedImageViewFilled(systemName: "arrow.counterclockwise")
       RoundedImageViewStroked(systemName: "list.dash")
       RoundedRectTextView(text: "100")
+      RoundedTextView(text: "1")
     }
   }
 }
--- Bullseye/Views/TextViews.swift
+++ Bullseye/Views/TextViews.swift
@@ -83,6 +83,30 @@ struct ButtonText: View {
   }
 }
 
+struct ScoreText: View {
+  var score: Int
+
+  var body: some View {
+    Text(String(score))
+      .bold()
+      .kerning(-0.2)
+      .foregroundColor(Color("TextColor"))
+      .font(.title3)
+  }
+}
+
+struct DateText: View {
+  var date: Date
+
+  var body: some View {
+    Text(date, style: .time)
+      .bold()
+      .kerning(-0.2)
+      .foregroundColor(Color("TextColor"))
+      .font(.title3)
+  }
+}
+
 struct TextViews_Previews: PreviewProvider {
   static var previews: some View {
     VStack {
@@ -92,6 +116,8 @@ struct TextViews_Previews: PreviewProvider {
       LabelText(text: "Score")
       BodyText(text: "You scored 200 Points\n 🎯🎯🎯")
       ButtonText(text: "Start New Round")
+      ScoreText(score: 459)
+      DateText(date: Date())
     }
     .padding()
   }
